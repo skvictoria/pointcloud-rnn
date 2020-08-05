97168bf42de040de8fd5d263ac35cd747541bbac
Seulgi Kim <40836553+skvictoria@users.noreply.github.com>
2020-08-05 17:28:31 +0900

    Create dataAugmentation.py

---
+++ dataAugmentation.py
@@ -0,0 +1,42 @@
+import numpy as np
+from scipy.interpolate import CubicSpline      # for warping
+from transforms3d.axangles import axangle2mat  # for rotation
+
+def DA_Jitter(X, sigma=0.05):
+    myNoise = np.random.normal(loc=0, scale=sigma, size=X.shape)
+    return X+myNoise
+
+def DA_Scaling(X, sigma=0.1):
+    scalingFactor = np.random.normal(loc=1.0, scale=sigma, size=(1,X.shape[1])) # shape=(1,3)
+    myNoise = np.matmul(np.ones((X.shape[0],1)), scalingFactor)
+    return X*myNoise
+
+def GenerateRandomCurves(X, sigma=0.2, knot=4):
+    xx = (np.ones((X.shape[1],1))*(np.arange(0,X.shape[0], (X.shape[0]-1)/(knot+1)))).transpose()
+    yy = np.random.normal(loc=1.0, scale=sigma, size=(knot+2, X.shape[1]))
+    x_range = np.arange(X.shape[0])
+    cs_x = CubicSpline(xx[:,0], yy[:,0])
+    cs_y = CubicSpline(xx[:,1], yy[:,1])
+    cs_z = CubicSpline(xx[:,2], yy[:,2])
+    return np.array([cs_x(x_range),cs_y(x_range),cs_z(x_range)]).transpose()
+
+def DistortTimesteps(X, sigma=0.2):
+    tt = GenerateRandomCurves(X, sigma) # Regard these samples aroun 1 as time intervals
+    tt_cum = np.cumsum(tt, axis=0)        # Add intervals to make a cumulative graph
+    # Make the last value to have X.shape[0]
+    t_scale = [(X.shape[0]-1)/tt_cum[-1,0],(X.shape[0]-1)/tt_cum[-1,1],(X.shape[0]-1)/tt_cum[-1,2]]
+    tt_cum[:,0] = tt_cum[:,0]*t_scale[0]
+    tt_cum[:,1] = tt_cum[:,1]*t_scale[1]
+    tt_cum[:,2] = tt_cum[:,2]*t_scale[2]
+    return tt_cum
+
+
+def DA_TimeWarp(X, sigma=0.2):
+    tt_new = DistortTimesteps(X, sigma)
+    X_new = np.zeros(X.shape)
+    x_range = np.arange(X.shape[0])
+    X_new[:,0] = np.interp(x_range, tt_new[:,0], X[:,0])
+    X_new[:,1] = np.interp(x_range, tt_new[:,1], X[:,1])
+    X_new[:,2] = np.interp(x_range, tt_new[:,2], X[:,2])
+    return X_new
+
