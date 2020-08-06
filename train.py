8c1d6076e22c27e0c38812196aefa9a0f8a3cce5
Seulgi Kim <40836553+skvictoria@users.noreply.github.com>
2020-08-07 03:00:57 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,12 +1,7 @@
 # train.py
 
 import pandas as pd
-# import tensorflow as tf
-from tensorflow.keras.layers import SimpleRNN, Embedding, Dense, LSTM
-from tensorflow.keras.layers import Conv1D
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.models import load_model
+#import tensorflow as tf
 from dataProcess import *
 from model import *
 import matplotlib
@@ -27,24 +22,25 @@ learning_rate = 0.01  #
 lambda_loss_amount = 0.0015  #
 
 batch_size = 8
-display_iter = 1000  # show test set accuracy during training
+display_iter = 100  # show test set accuracy during training
 
-def extract_batch_size(_train, step, batch_size):
+
+def extract_batch_size(_train, step, batchsize):
     shape = list(_train.shape)
-    shape[0] = batch_size
+    shape[0] = batchsize
     batch_s = np.empty(shape)
 
-    for i in range(batch_size):
+    for i in range(batchsize):
         # Loop index
-        index = ((step - 1) * batch_size + i) % len(_train)
+        index = ((step - 1) * batchsize + i) % len(_train)
         batch_s[i] = _train[index]
 
     return batch_s
 
 
-def one_hot(y_, n_classes=n_classes):
+def one_hot(y_, num_classes=n_classes):
     y_ = y_.reshape(len(y_))
-    return np.eye(n_classes)[np.array(y_, dtype=np.int32)]  # Returns FLOATS
+    return np.eye(num_classes)[np.array(y_, dtype=np.int32)]  # Returns FLOATS
 
 
 LABELS = [
@@ -53,44 +49,41 @@ LABELS = [
     "TURN"
 ]
 
-X_train = np.empty([0, cnt, 3])
-X_test = np.empty([0, cnt, 3])
+X_train = np.empty([0, cnt, 4])
+X_test = np.empty([0, cnt, 4])
 y_train = np.empty([0, ])
 y_test = np.empty([0, ])
 tmp_dataframe = pd.DataFrame()
 
-label_df = pd.read_csv('/content/gdrive/My Drive/labeling.csv', index_col=0)
+label_df = pd.read_csv('./labeling.csv', index_col=0)
 for index, row in label_df.iterrows():
+    fill_turn = False
+    fill_lnchn = False
     tracknum = index
-    if (np.isnan(row['start_lnchn'])):
-        lanechng = (0, 0, 0)
+    if not np.isnan(row['start_turn']):
         turn = (1, row['start_turn'], row['fin_turn'])
-        print(lanechng, turn)
-    elif (np.isnan(row['start_turn'])):
+        fill_turn = True
+    if not np.isnan(row['start_lnchn']):
         lanechng = (1, row['start_lnchn'], row['fin_lnchn'])
-        turn = (0, 0, 0)
-        print(lanechng, turn)
-    tmp_dataframe = pd.concat([tmp_dataframe, dataAssembly(tracknum, lanechng, turn)])
+        fill_lnchn = True
+    if(fill_lnchn == False): lanechng = (0,0,0)
+    if(fill_turn == False): turn = (0,0,0)
+
+    print(lanechng, turn)
+    tmp_dataframe = pd.concat([tmp_dataframe, dataAssembly(tracknum,lanechng, turn,'./data/')])
 
 print(tmp_dataframe)
-plt.figure()
-tmp_dataframe['x'].hist()
-plt.show()
-plt.figure()
-tmp_dataframe['y'].hist()
-plt.show()
-plt.figure()
-tmp_dataframe['yaw'].hist()
-plt.show()
 
-X_train, y_train, X_test, y_test = dataProcess_main(tmp_dataframe, '', cnt, span)
+X_train, y_train, X_test, y_test = dataProcess_main(tmp_dataframe, cnt, span)
+
+
 
 training_data_count = len(X_train)
 test_data_count = len(X_test)
 n_steps = len(X_train[0])  # number of timestamp
 n_input = len(X_train[0][0])  # how many input parameters per timestamp
 
-training_iters = training_data_count * 300  # Loop 300 times on the dataset
+training_iters = training_data_count * 100  # Loop 300 times on the dataset
 
 # shape, normalization
 print("(X shape, y shape, every X's mean, every X's standard deviation)")
@@ -120,7 +113,7 @@ biases = {
     'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
-pred = LSTM_RNN(x, weights, biases, n_input, n_steps)
+pred = LSTM_RNN(x, weights, biases, n_input, n_steps, n_hidden)
 
 # Loss, optimizer and evaluation
 l2 = lambda_loss_amount * sum(
@@ -145,7 +138,7 @@ sess.run(init)
 # Perform Training steps with "batch_size" amount of example data at each loop
 step = 1
 while step * batch_size <= training_iters:
-    batch_xs =         extract_batch_size(X_train, step, batch_size)
+    batch_xs = extract_batch_size(X_train, step, batch_size)
     batch_ys = one_hot(extract_batch_size(y_train, step, batch_size))
 
     # Fit training using batch data
