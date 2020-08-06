2b87b24c05c07b0feb1b09ddd837841d30369783
Seulgi Kim <40836553+skvictoria@users.noreply.github.com>
2020-08-07 03:01:58 +0900

    Update dataProcess.py

--- dataProcess.py
+++ dataProcess.py
@@ -15,17 +15,22 @@ lambda_loss_amount = 0.0015  #
 batch_size = 8
 display_iter = 1000  # show test set accuracy during training
 
+
+
 # 데이터 받아서 csv에 정답 넣어줌
-def dataAssembly(tracknum,path='',  lanechng=(0, 0, 0), turn=(0, 0, 0)):
+def dataAssembly(tracknum, lanechng, turn, path=''):
     tmpdf = pd.read_csv(path+'{}.csv'.format(tracknum), index_col=0)
     tmpdf["ans"] = 0
+    ans1 = ans2 = 0
     if (lanechng[0] == 1):
         tmpdf.at[lanechng[1]:lanechng[2], 'ans'] = 1
-    elif (turn[0] == 1):
+        print('1 updated')
+    if (turn[0] == 1):
         tmpdf.at[turn[1]:turn[2], 'ans'] = 2
+        print('2 updated')
     tmpdf = tmpdf.dropna(axis=0)
     return tmpdf
-
+'''
 # train, test data 나눠줌
 def split(data, y_data):
     n_train = int(len(data) * 0.8)
@@ -36,10 +41,41 @@ def split(data, y_data):
     X_train = np.array(data[:n_train])
     y_train = np.array(y_data[:n_train])
 
-    print('train 개수 :' , n_train)
+    print('train 개수 :', n_train)
     print('test 개수 :', n_test)
 
     return X_train, y_train, X_test, y_test
+'''
+
+# scailing
+def minmaxScailing(dataframe, xmean=0, xstd=0, ymean=0, ystd=0, vmean=0, vstd=0):
+  if xstd==0 and ystd==0:
+    x = dataframe[0]
+    print(x)
+    x_mean = x.mean()
+    print(x_mean)
+    x_std = x.std()
+    print(x_std)
+    x = (x-x_mean)/x_std
+    print(x)
+    y = dataframe[1]
+    y_mean = y.mean()
+    y_std = y.std()
+    y = (y-y_mean)/y_std
+    v = dataframe[2]
+    v_mean = v.mean()
+    v_std = v.std()
+    v = (v-v_mean)/v_std
+  else:
+    x = dataframe[0]
+    x = (x-xmean)/xstd
+    y = dataframe[1]
+    y = (y-ymean)/ystd
+    v = dataframe[2]
+    v = (v-vmean)/vstd
+    x_mean=x_std=y_mean=y_std=v_mean=v_std = 0
+  return pd.concat([x,y,v,dataframe[3],dataframe[4]], axis=1), x_mean, x_std,y_mean, y_std, v_mean, v_std
+  
 
 # dataframe을 받아서 0,1,2 폴더로 나눠줌
 def dataProcess_main(tmpdf, cnt, span):
@@ -50,12 +86,12 @@ def dataProcess_main(tmpdf, cnt, span):
         tmplist = []
         for j in range(i, cnt + i):
             tmplist.append(j)
-        if (tmpdf.iloc[i + cnt + span, 3] == 0):
-            normal_data.append(tmpdf.iloc[tmplist, [0, 1, 2]].to_numpy())
-        elif (tmpdf.iloc[i + cnt + span, 3] == 1):
-            lanechng_data.append(tmpdf.iloc[tmplist, [0, 1, 2]].to_numpy())
-        elif (tmpdf.iloc[i + cnt + span, 3] == 2):
-            turn_data.append(tmpdf.iloc[tmplist, [0, 1, 2]].to_numpy())
+        if (tmpdf.iloc[i + cnt + span, 5] == 0):
+            normal_data.append(tmpdf.iloc[tmplist, [0, 1, 2, 3, 4]].to_numpy())
+        elif (tmpdf.iloc[i + cnt + span, 5] == 1):
+            lanechng_data.append(tmpdf.iloc[tmplist, [0, 1, 2, 3, 4]].to_numpy())
+        elif (tmpdf.iloc[i + cnt + span, 5] == 2):
+            turn_data.append(tmpdf.iloc[tmplist, [0, 1, 2, 3, 4]].to_numpy())
         else:
             continue
 
@@ -73,16 +109,45 @@ def dataProcess_main(tmpdf, cnt, span):
     y_data = np.append(y_data, np.full((1, len(lanechng_data)), 1))
     y_data = np.append(y_data, np.full((1, len(turn_data)), 2))
 
-    X_train, X_test, y_train, y_test = train_test_split(normal_data+lanechng_data+turn_data, y_data, test_size=0.2, shuffle=True, stratify=grp, random_state = 1004)
-    print(X_train.shape)
-    print(X_test.shape)
-    print(y_train.shape)
-    print(y_test.shape)
+    X_train, X_test, y_train, y_test = train_test_split(normal_data+lanechng_data+turn_data, y_data, test_size=0.2,
+                                                        shuffle=True, stratify=y_data, random_state = 1004)
+    print(X_train[0].shape)
+    print(type(X_train[0]))
+    print(X_test[0].shape)
+    print(y_train[0])
+    print(y_test[0])
 
-    x_scaler = MinMaxScaler()
-    x_train_scaled = x_scaler.fit_transform(X_train)
-    print("Min:", np.min(x_train_scaled))
-    print("Max:", np.max(x_train_scaled))
-    x_test_scaled = x_scaler.transform(X_test)
+    
+    forminmax = X_train[0].tolist()
+    for i in range(1, len(X_train)):
+      forminmax += X_train[i].tolist()
 
+    fortest = X_test[0].tolist()
+    for i in range(1, len(X_test)):
+      fortest += X_test[i].tolist()
+
+
+    x_train_raw, xmean, xstd, ymean, ystd, vmean, vstd = minmaxScailing(pd.DataFrame(forminmax))
+    x_train_raw = x_train_raw.to_numpy()
+    x_train_scaled = np.empty([0, cnt, 5])
+    
+    for i in range(int(len(x_train_raw)/cnt)):
+      i = i*cnt
+      x_tmp = np.empty([0,5])
+      for j in range(i, i+cnt):
+        x_tmp = np.append(x_tmp, [x_train_raw[j]], axis = 0)
+      x_train_scaled = np.append(x_train_scaled, [x_tmp], axis=0)
+    x_train_scaled.tolist()
+
+    x_test_raw, _, _, _, _, _, _ = minmaxScailing(pd.DataFrame(fortest), xmean, xstd, ymean, ystd, vmean, vstd)
+    x_test_raw = x_test_raw.to_numpy()
+    x_test_scaled = np.empty([0, cnt, 5])
+    
+    for i in range(int(len(x_test_raw)/cnt)):
+      i = i*cnt
+      x_tmp = np.empty([0,5])
+      for j in range(i, i+cnt):
+        x_tmp = np.append(x_tmp, [x_test_raw[j]], axis = 0)
+      x_test_scaled = np.append(x_test_scaled, [x_tmp], axis=0)
+    x_test_scaled.tolist()
     return x_train_scaled, y_train, x_test_scaled, y_test
